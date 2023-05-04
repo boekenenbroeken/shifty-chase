69be31139df85b8a674a88fda1af8e3d02d6cf58
Ingrid <almost.bergman@gmail.com>
2023-05-04 15:24:20 +0200

    fix semantic error

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,9 +3,7 @@ import { SlidingPuzzle } from 'components/SlidingPuzzle';
 const Home = () => {
   return (
     <div>
-      <h1>
-        <SlidingPuzzle />
-      </h1>
+      <SlidingPuzzle />
     </div>
   );
 };
