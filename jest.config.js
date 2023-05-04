50bbe3d898b0516e63fdb272df5fc2aa1eb1ce47
Ingrid <almost.bergman@gmail.com>
2023-05-04 12:26:47 +0200

    add react testing library

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>/support/setupTests.js'],
+  testEnvironment: 'jsdom',
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!<rootDir>/node_modules/']
 };
