c2f0c027fea81bb63ed5225f657e3d4c36fcf8ff
Ingrid <almost.bergman@gmail.com>
2023-05-09 11:17:14 +0200

    add congrats component

---
+++ src/components/Congrats/Congrats.style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const StyledCongrats = styled.p`
+  font-family: 'Press Start 2P', serif;
+  margin: 12px 0 24px 0;
+`;
+
+export { StyledCongrats };
---
+++ src/components/Congrats/index.ts
@@ -0,0 +1,1 @@
+export { Congrats } from './Congrats';
